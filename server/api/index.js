c82750b1c0f7f934d402cf5e593985b07ca04b29
abhi8114 <08114akv@gmail.com>
2024-08-30 19:12:18 +0530

    database connection

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
 
 const app = express();
 
