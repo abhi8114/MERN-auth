8e28fb2836cfe3bceb1f5653d68feddc9e4f416d
abhi8114 <08114akv@gmail.com>
2024-09-06 12:04:21 +0530

    user route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,2 +1,15 @@
 import express from 'express';
 
+import {
+    deletUser,
+    updateUser,
+} from '../controllers/user.controller.js';
+
+import { verifyToken } from '../utils/verify.token.js';
+
+const router = express.Router();
+
+router.post('/update/:id', verifyToken, updateUser);
+router.delete('/delete/:id', verifyToken, deletUser);   
+
+export default router;
